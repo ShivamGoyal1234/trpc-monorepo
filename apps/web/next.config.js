9b7d0de3e30e60028cbd1791d53a328af3dcbe0b
Shivam Goyal <43113565+ShivamGoyal1234@users.noreply.github.com>
2026-05-27 23:42:01 +0530

    ignore unnecessary in ignore and fix  copy  app

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  transpilePackages: ["api"],
+  transpilePackages: ["api", "@repo/schemas"],
 };
 
 export default nextConfig;
